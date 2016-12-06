64ae95a87ef60a3f4ab64521619c08fef85679b7
github username <github email address>
2016-12-06 11:55:16 -0500

    api key variable

--- js/app/services/Movie.js
+++ js/app/services/Movie.js
@@ -4,7 +4,7 @@ function Movie($http) {
 
 
   this.getDirectorId = function () {
-    return $http.get('https://api.themoviedb.org/3/search/person?api_key=a671a47592d30a7f94478bc9b59410d9&language=en-US&query=ingmar%20bergman&page=1')
+    return $http.get('https://api.themoviedb.org/3/search/person?api_key=' + moviekey + '&language=en-US&query=ingmar%20bergman&page=1')
   }
 
 }
