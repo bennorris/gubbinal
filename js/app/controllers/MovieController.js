9c7f72fe46d8a58fef8481a83a6b5c8825e76eb4
github username <github email address>
2016-12-06 16:08:23 -0500

    added button for dropdown with more info

--- js/app/controllers/MovieController.js
+++ js/app/controllers/MovieController.js
@@ -2,9 +2,24 @@ function MovieController($scope, GetDirectorId, DirectorCredits, ListDirectorFil
   var ctrl = this;
   $scope.searchTerm = '';
   $scope.searchId = '';
+  $scope.moreInfo = "display: none;";
   ctrl.data = [];
   ctrl.films = []; //this array = list of films w id included
   ctrl.reviews = [];
+  $scope.buttonText = "see more";
+  var count = 0;
+
+  $scope.showMore = function() {
+    if (count == 0) {
+    $scope.moreInfo = "display: block;"
+    $scope.buttonText = "see less"
+    count+=1;
+  } else if (count == 1) {
+    $scope.moreInfo = "display: none;"
+    $scope.buttonText = "see more"
+    count -=1;
+  }
+}
 
  $scope.testing = function() {
   GetDirectorId
